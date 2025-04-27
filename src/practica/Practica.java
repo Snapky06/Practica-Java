19b230a721e828883e9db95d32f3883bc612f402
Snapky06 <saidnapky@gmail.com>
2025-04-27 17:57:06 -0600

    Initial commit

---
+++ src/practica/Practica.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica;
+
+/**
+ *
+ * @author saidn
+ */
+public class Practica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+        
+        int width; 
+        int height;
+        int area;
+        double radius = 10.0;
+        double pi = 3.14;
+        boolean result = true ;
+       width = 8;
+               height = 12;
+                       area = 96;
+ 
+                       System.out.println("The value of width is "+ width);
+                       System.out.println("The value of height is "+ height);
+                       System.out.println("The value of area is "+ area);
+                       System.out.println("The value of radius is "+ radius);
+                       System.out.println("The value of width is "+ width);
+                       System.out.println("The value of pi is "+ pi);
+                       System.out.println("The value of result is "+ result);
+                       
+     int[] daysInMonths = {31,28,31,30,31,30,31,31,30,31,30,31};
+     String[] monthNames = {"January","February","March","April","May","June","July","August","September","October","November","Dicember",};
+     
+     System.out.println(monthNames[0] + " has " + daysInMonths[0]);
+     System.out.println(monthNames[1] + " has " + daysInMonths[1]);
+     System.out.println(monthNames[2] + " has " + daysInMonths[2]);
+     System.out.println(monthNames[3] + " has " + daysInMonths[3]);
+     System.out.println(monthNames[4] + " has " + daysInMonths[4]);
+     System.out.println(monthNames[5] + " has " + daysInMonths[5]);
+     System.out.println(monthNames[6] + " has " + daysInMonths[6]);
+     System.out.println(monthNames[7] + " has " + daysInMonths[7]);
+     System.out.println(monthNames[8] + " has " + daysInMonths[8]);
+     System.out.println(monthNames[9] + " has " + daysInMonths[9]);
+     System.out.println(monthNames[10] + " has " + daysInMonths[10]);
+     System.out.println(monthNames[11] + " has " + daysInMonths[11]);
+     
+    }
+}    
